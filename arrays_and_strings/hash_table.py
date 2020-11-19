2c0e7305cb4250a518e5201432ade3449a84ff96
Duong <mdnnguyen@ucdavis.edu>
2020-11-18 19:39:28 -0800

    Fix func names and reorder if statements for more Pythonic code

--- arrays_and_strings/hash_table.py
+++ arrays_and_strings/hash_table.py
@@ -13,30 +13,29 @@ class KeyValuePair:
 
 class HashTable:
     def __init__(self, size=16):
-        if not self.isValidSize(size):
+        if not self._isValidSize(size):
             raise ValueError("Invalid size value! Should be an integer greater than 0.")
-        self.table = [None] * size
-        for i in range(size):
-            self.table[i] = SinglyLinkedList()
+        self.table = [SinglyLinkedList() for __ in range(size)]
         self.size = size
 
-    def isValidSize(self, size):
+    def _isValidSize(self, size):
         return isinstance(size, int) and size > 0
 
-    def hash(self, key):
+    def _hash(self, key):
         '''
             Hash a given key and return the corresponding index in the table.
+            Todo: Figure out why hash function always return the same value.
         '''
         if isinstance(key, str):
-            return self.hashStr(key) % self.size
+            return self._hashStr(key) % self.size
         elif isinstance(key, int):
-            return self.hashInt(key) % self.size
+            return self._hashInt(key) % self.size
         elif isinstance(key, float):
-            return self.hashFloat(key) % self.size
+            return self._hashFloat(key) % self.size
         else:
             raise TypeError("Invalid key type!")
 
-    def hashStr(self, key):
+    def _hashStr(self, key):
         # Inspired by Python's string_hash function
         hashVal = 0
         bitShiftAmount = 7
@@ -46,13 +45,13 @@ class HashTable:
         hashVal = hashVal ^ len(key)
         return hashVal
 
-    def hashInt(self, key):
+    def _hashInt(self, key):
         bitShiftAmount = 4
         bitXorAmount = 62982
         multNum = 27
         return ((key ^ bitXorAmount) * multNum) << bitShiftAmount
 
-    def hashFloat(self, key):
+    def _hashFloat(self, key):
         bitShiftAmount = 8
         bitXorAmount = 53671
         multNum = 72
@@ -60,13 +59,12 @@ class HashTable:
         return (int(key * multNum / divNum) << bitShiftAmount) ^ bitXorAmount
 
     def insert(self, key, value):
-        hashIdx = self.hash(key)
+        hashIdx = self._hash(key)
         newKeyVal = KeyValuePair(key, value)
         list = self.table[hashIdx]
-        if list.search(newKeyVal) == None:
-            list.insert(newKeyVal)
-        else:
+        if list.search(newKeyVal) is not None:
             raise ValueError("Key already exists in table!")
+        list.insert(newKeyVal)
 
     def delete(self, key):
         pass
